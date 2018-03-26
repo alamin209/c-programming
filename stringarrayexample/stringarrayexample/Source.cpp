bd1ae9a1e5d702d25ec4193381234c272d8cc433
abc <you@example.com>
2018-03-26 19:38:34 +0600

    addin example of cheapter 9

--- stringarrayexample/stringarrayexample/Source.cpp
+++ stringarrayexample/stringarrayexample/Source.cpp
@@ -1,36 +1,48 @@
 #include<stdio.h>
-void main() 
+#include<conio.h>
+#include<math.h>
+//void main() 
+//{
+//	char ch[7] = "alamin";
+//	int i=0;
+//	char *ptr = ch;
+//	printf("%s", ch);
+//
+//	char name[25];
+//	printf("Take the name");
+//	scanf_s("%s", name);
+//	printf(" hello %s", name);
+//	printf("no value........................");
+//	while (*ptr!='\0')
+//	{
+//		printf("%c", *ptr);
+//		*ptr++;
+//	}
+//	//must be comment out one if you want to see another
+//	/*while (i<=6)
+//	{
+//		printf("%c", *(ptr+i));
+//		i++;
+//	}
+//
+//	printf("\n");*/
+//
+//	char name[25];
+//	printf("Enter your full name ");
+//	scanf_s("%[^\n]s", &name);
+//	printf("balchal print hoy na %s", name);
+//
+//}
+
+/// here is an example of gets and puts 
+
+void main()
 {
-	char ch[7] = "alamin";
-	int i=0;
-	char *ptr = ch;
-	printf("%s", ch);
-
 	char name[25];
-	printf("Take the name");
-	scanf_s("%s", name);
-	printf(" hello %s", name);
-	printf("no value........................");
-	while (*ptr!='\0')
-	{
-		printf("%c", *ptr);
-		*ptr++;
-	}
-	//must be comment out one if you want to see another
-	/*while (i<=6)
-	{
-		printf("%c", *(ptr+i));
-		i++;
-	}
+	printf("Enter the name");
+	gets_s(name);
+	puts("Hello");
+	puts(name);
 
-	printf("\n");*/
-
-
-	//char array[100];
-
-	//printf("Enter a string\n");
-	//scanf_s("%s", array);
-
-	//printf("Your entered string: %s\n", array);
 
 }
